9a3fad155f267fd9a0327c17fc5106b484e61448
christian <cristian14021997@gmail.com>
2021-03-24 10:58:42 -0500

    Adding styles in button

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,6 +7,26 @@ const paddings = {
   small: "4px 8px",
 };
 
+const StyledButton = styled.button(
+  (props) => css`
+    padding: ${paddings[props.size]};
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    box-sizing: border-box;
+    border-radius: 16px;
+    border: 1px solid #f48fb1;
+    background-color: #ffffff;
+    color: #616161;
+    font-size: 14px;
+    line-height: 24px;
+    cursor: pointer;
+    &:hover {
+      background: gba(244, 143, 177, 0.15);
+    }
+  `
+);
+
 function Button({ children, size = "medium", onClick }) {
   return (
     <StyledButton size={size} onClick={onClick}>
