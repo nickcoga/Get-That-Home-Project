81154c8e6c51cfa2ef03e4218957bc6f9479db79
Nick Correa <nickcorreagarcia@gmail.com>
2021-03-24 16:47:31 -0500

    Add icons in footerlanding.js

--- src/components/UI/FooterLanding.js
+++ src/components/UI/FooterLanding.js
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 import { colors } from "../styles/ColorStyles";
 import Text from "../styles/Heading";
+import Icons from "./Icons";
 
 export default function FooterLanding() {
   return (
@@ -9,22 +10,28 @@ export default function FooterLanding() {
         <Text type="SemiBold" size="Body2">
           © 2021 - Find That Home
         </Text>
-        <Text type="SemiBold" size="Body2">
-          Source Code
-        </Text>
+        <div>
+          <DivTwo>
+            <Text type="SemiBold" size="Body2">
+              Source Code
+            </Text>
+          </DivTwo>
+          <DivTree>
+            <Icons type="ruby" />
+            <Text type="SemiBold" size="Body2">
+              Ruby on Rails REST API
+            </Text>
+            <Icons type="react" />
+            <Text type="SemiBold" size="Body2">
+              React Responsive SPA
+            </Text>
+          </DivTree>
+        </div>
+
         <Text type="SemiBold" size="Body2">
           Codeable - Cohort 3 Final Project
         </Text>
       </DivOne>
-
-      <DivTwo>
-        <Text type="SemiBold" size="Body2">
-          Ruby on Rails REST API
-        </Text>
-        <Text type="SemiBold" size="Body2">
-          React Responsive SPA
-        </Text>
-      </DivTwo>
     </Footer>
   );
 }
@@ -32,22 +39,31 @@ export default function FooterLanding() {
 const Footer = styled.footer`
   display: flex;
   flex-direction: column;
+  align-items: center;
   position: fixed;
   bottom: 0;
   left: 0;
   background: ${colors.Background};
   width: 100%;
-  padding: 25px 0;
-  border: 2px solid black;
+  padding: 10px 0;
 `;
 
 const DivOne = styled.div`
+  width: 80%;
   display: flex;
   justify-content: space-around;
 `;
 
 const DivTwo = styled.div`
+  width: 395px;
   display: flex;
   justify-content: center;
-  gap: 10px;
+  align-items: center;
+`;
+
+const DivTree = styled.div`
+  width: 395px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 `;
