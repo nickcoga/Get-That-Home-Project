3df5d7d454ad8cb28a3899352fb2876070112d74
Nick Correa <nickcorreagarcia@gmail.com>
2021-03-24 11:35:39 -0500

    Create file ColorsStyles.js

---
+++ src/components/styles/ColorStyles.js
@@ -0,0 +1,13 @@
+export const colors = {
+  Pink: "#F48FB1",
+  LightPink: "#FFC1E3",
+  DarkPink: "#BF5F82",
+  Gray: "#616161",
+  LightGray: "#8E8E8E",
+  DarkGray: "#373737",
+  Background: "#F5F5F6",
+  BackgroundLight: "#E1E2E1",
+  White: "#FFFFFF",
+  ShallowPink: "rgba(244, 143, 177, 0.15)",
+  ShallowGray: "rgba(97, 97, 97, 0.15);",
+};
