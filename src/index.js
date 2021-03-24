fb67b3a93cdc07099a2ef7e9fb2de86b8132fcc2
christian <cristian14021997@gmail.com>
2021-03-24 13:07:51 -0500

    Initialize inputs component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Button from "./components/UI/Button";
+import Inputs from "./components/UI/Inputs";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <Button size="small">Button</Button>
+    <Inputs />
   </React.StrictMode>,
   document.getElementById("root")
 );
