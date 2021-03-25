3a2600d8cefd30501946a99564872b92a47eafdd
Abel AR <abel_john_14@hotmail.com>
2021-03-25 12:10:23 -0500

    Add new restrictions for inputs

--- src/components/UI/ModalSalary.js
+++ src/components/UI/ModalSalary.js
@@ -1,5 +1,5 @@
 import { InputNumber } from "./Inputs";
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 import styled from "@emotion/styled";
 import formReducer from "../../reducers/formReducer";
 
@@ -12,6 +12,8 @@ const ContainerSalary = styled.div`
 `;
 
 const ModalSalary = () => {
+  const [cannotBeLetters, setCannotBeLetters] = useState(false);
+  const [testNumbers, setTestNumbers] = useState(false);
   const [state, dispatch] = useReducer(formReducer, {
     min: "",
     max: "",
@@ -19,27 +21,47 @@ const ModalSalary = () => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
+
+    const onlyNumbers = /^[0-9]*$/;
+    if(!(onlyNumbers.test(e.target.value))) {
+      return setCannotBeLetters(true);
+    } else {
+     setCannotBeLetters(false);
+    }
+
+    if(e.target.value.length===10) {
+      return setTestNumbers(true);
+    } else {
+      setTestNumbers(false);
+    }
+
     dispatch({ type: "CHANGE_FIELD", payload: { name, value } });
   };
 
   return (
-    <ContainerSalary>
-      <InputNumber
-        label="min"
-        placeholder="min"
-        name="min"
-        value={state.min}
-        onChange={handleChange}
-      />
-      <p>-</p>
-      <InputNumber
-        label="max"
-        placeholder="max"
-        name="max"
-        value={state.max}
-        onChange={handleChange}
-      />
-    </ContainerSalary>
+    <>
+      {cannotBeLetters && <p>This field can not read letter or words</p>}
+      {testNumbers && <p>You cannot exceed the maximum number, please enter a number within the allowed range</p>}
+      <ContainerSalary>
+        <InputNumber
+          label="min"
+          placeholder="min"
+          name="min"
+          value={state.min}
+          onChange={handleChange}
+          maxLength="10"
+        />
+        <p>-</p>
+        <InputNumber
+          label="max"
+          placeholder="max"
+          name="max"
+          value={state.max}
+          onChange={handleChange}
+          maxLength="10"
+        />
+      </ContainerSalary>
+    </>
   );
 };
 
