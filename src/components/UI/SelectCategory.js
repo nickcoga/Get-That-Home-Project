ec131128096708275f8511cf95da46c1965de32b
juancarlos <juancarlosenrique103@gmail.com>
2021-03-26 07:41:44 -0500

    Add styles to button select :zap:

--- src/components/UI/SelectCategory.js
+++ src/components/UI/SelectCategory.js
@@ -6,7 +6,7 @@ function SelectCategory({categories}) {
   return (
     <StyledContainer>
       <StyledInput>
-        <button>Select a category</button>
+        <span>Select a category</span>
         <Icons type="down" color={colors.DarkGray} />
       </StyledInput>
       <StyledSelects>
@@ -23,11 +23,35 @@ function SelectCategory({categories}) {
 }
 
 const StyledContainer = styled.div`
-
+  font-family: sans-serif;
 `;
 
 const StyledInput = styled.div`
-
+  box-sizing: border-box;
+  width: 250px;
+  padding: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 12px;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 20px;
+  border: 1px solid ${colors.Pink};
+  border-radius: 8px;
+  color: ${colors.Gray};
+  &:hover {
+    cursor: pointer;
+    color: ${colors.DarkGray};
+    background-color: #F5518810;
+  }
+  span {
+    font-size: 14px;
+    flex: 1;
+    border: none;
+  }
+  
 `;
 
 const StyledSelects = styled.div`
