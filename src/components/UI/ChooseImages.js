a09cb7e377566b7202861455ab9801d419e9012c
juancarlos <juancarlosenrique103@gmail.com>
2021-03-26 16:36:51 -0500

    Add functionality load images inside of windows :zap:

--- src/components/UI/ChooseImages.js
+++ src/components/UI/ChooseImages.js
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import styled from "@emotion/styled";
 import { colors } from "../styles/ColorStyles";
 import Icons from "./Icons";
 
 function ChooseImages({ onChange, name='image', label='image' }) {
+  const [photos, setPhotos] = useState([]);
+  const [photos64, setPhotos64] =  useState([]);
   const handleClick = () => {
     console.log("click");
     let chooseInput = document.getElementById("choose-file-input");
@@ -10,10 +13,30 @@ function ChooseImages({ onChange, name='image', label='image' }) {
     chooseInput.style.color = colors.DarkGray;
   };
 
-  // const handleChange = ({target}) => {
-  //   const { name, value } = target;
-  //   console.log(name, value);
-  // }
+  const sendData = (e) => {
+    const { value }= e.target;
+    let image64 = '';
+    let file = e.target.files[0];
+    let reader = new FileReader();
+    reader.onloadend = () => {
+      image64 = reader.result;
+      setPhotos64(prev => {
+        if(!prev.includes(image64)) {
+          return [image64, ...prev]
+        } 
+        return prev;
+      })
+    }
+    reader.readAsDataURL(file);
+
+    setPhotos(prev => {
+      if(prev.includes(value)) {
+        return prev.filter(photo => photo !== value);
+      } else {
+        return [value, ...prev]
+      }
+    })
+  }
   
   return (
     <StyledContainer>
@@ -27,18 +50,29 @@ function ChooseImages({ onChange, name='image', label='image' }) {
           id="choose-file-input"
           name={name}
           type="file"
-          onChange={onChange}
-          accept="j"
+          onChange={sendData}
+          multiple
+          accept="image/*"
         />
       </div>
       <p>Only images, max 5MB</p>
       <StyleContainerPhotos>
-        <div className="no-photos">
-          <p>No photos yet.</p>
-        </div>
-        <div className="photo-container">
-
-        </div>
+        {photos64.length > 0 ?
+          <div className="photos-container">
+            {photos64.map((photo, index )=> (
+              <div key={index} className="photo-container">
+                <div className="button-close">
+                  <Icons type="close" color={colors.DarkGray} />
+                </div>
+                <img className="photo-property" src={photo} alt={"Photo" + index}/>
+              </div>
+            ))}
+          </div>:
+          <div className="no-photos">
+            <p>No photos yet.</p>
+          </div>
+        }
+        
       </StyleContainerPhotos>
     </StyledContainer>
   );
@@ -78,7 +112,7 @@ const StyledContainer = styled.div`
 
   &  button {
     background-color: ${({ disabled }) =>
-      disabled ? '#61616147' : colors.Pink};
+    disabled ? '#61616147' : colors.Pink};
     display: flex;
     align-items: center;
     gap: 3px;
@@ -114,8 +148,9 @@ const StyledContainer = styled.div`
 `;
 
 const StyleContainerPhotos = styled.div`
-  height: 140px;
   background: #F5F5F6;
+  padding: 8px;
+  box-sizing: border-box;
   & > .no-photos {
     width: 120px;
     height: 120px;
@@ -130,6 +165,50 @@ const StyleContainerPhotos = styled.div`
     line-height: 16px;
     color: ${colors.DarkGray};
   }
+
+  .photos-container {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+  }
+
+  .photo-container {
+    box-sizing: border-box;
+    width: 140px;
+    height: 160px;
+    padding: 2px 0px;
+    border-radius: 8px;
+    display: flex;
+    background: ${colors.BackgroundLight};
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 16px;
+    color: ${colors.DarkGray};
+    overflow: hidden;
+  }
+
+  .button-close {
+    box-sizing: border-box;
+    width: 100%;
+    padding: 2px 10px;
+    display: flex;
+    justify-content: flex-end;
+    font-size: 16px;
+    color: ${colors.Gray};
+    & > svg:hover {
+      cursor: pointer;
+      color: #000; 
+    }
+  }
+  .photo-property {
+    width: 100%;
+    height: 80%;
+  }
+
 `;
 
 export default ChooseImages;
