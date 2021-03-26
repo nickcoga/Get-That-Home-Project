c9e87176345075dd7855f6f62604da0c29aec475
juancarlos <juancarlosenrique103@gmail.com>
2021-03-26 11:03:36 -0500

    Add styled to component :zap:

--- src/components/UI/ToggleOperation.js
+++ src/components/UI/ToggleOperation.js
@@ -5,14 +5,16 @@ function ToggleOperation() {
   return (
     <StyledContainer>
       <h2>Operation type</h2>
-      <label>
-        <span>Rent</span>
-        <input type="radio" name="operation" value="rent" />
-      </label>
-      <label>
-        <span>Sale</span>
-        <input type="radio" name="operation" value="sale" />
-      </label>
+      <div className="operations">
+        <label>
+          <span>Rent</span>
+          <input type="radio" name="operation" value="rent" />
+        </label>
+        <label>
+          <span>Sale</span>
+          <input type="radio" name="operation" value="sale" />
+        </label>
+      </div>
     </StyledContainer>
   );
 }
@@ -22,11 +24,58 @@ const StyledContainer = styled.div`
   & > h2 {
     font-style: normal;
     font-weight: normal;
-    font-size: 10px;
+    font-size: 11px;
     line-height: 16px;
     color: ${colors.DarkGray};
+    text-transform: uppercase;
+    margin: 0;
+    margin-bottom: 4px;
+    color: ${colors.Gray};
   }
-  
+  & > .operations {
+    width: 100px;
+    border: 1px solid ${colors.Gray};
+    display: flex;
+    align-items: center;
+    align-content: center;
+    justify-content: space-between;
+    border-radius: 8px;
+    overflow: hidden;
+    label {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+      font-weight: normal;
+      font-size: 14px;
+      line-height: 20px;
+      color: ${colors.Gray};
+      width: 50px;
+      height: 35px;
+      border-right: 1px solid ${colors.Gray};
+      transition: all 250ms ease-in-out;
+    }
+
+    label:last-child {
+      border-right: none;
+    }
+
+    label:hover {
+      color: ${colors.White};
+      cursor: pointer;
+      background: ${colors.Pink};
+    }
+
+    .active-check {
+      color: ${colors.White};
+      cursor: pointer;
+      background: ${colors.Pink};
+    }
+
+    label > input {
+      display: none;
+    }
+  }
 `;
 
 export default ToggleOperation;
