5c714fb3b6f9a53fc8720e495b12a73510f1cdeb
juancarlos <juancarlosenrique103@gmail.com>
2021-03-29 13:05:15 -0500

    Add styles to component :zap:

--- src/components/UI/Pagination.js
+++ src/components/UI/Pagination.js
@@ -1,9 +1,10 @@
 import styled from "@emotion/styled";
+import { colors } from "../styles/ColorStyles";
 import Icons from "./Icons";
 
 const numbers = [1, 2, 3, 4, 5, 6, 7];
 function Pagination({pages = numbers}) {
-  
+
   return (
     <StyledContainer>
       <Icons type="previous" />
@@ -19,10 +20,43 @@ function Pagination({pages = numbers}) {
 
 const StyledContainer = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
 `;
 
 const StyleContainerNumbers = styled.div`
+  display: flex;
+  gap: 5px;
+  justify-content: space-between;
+  font-family: sans-serif;
+  width: 180px;
+  overflow: hidden;
+  & > span {
+    box-sizing: border-box;
+    width: 32px;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 20px;
+    color: ${colors.Gray};
+    padding: 5px 12px;
+    border: 1px solid rgba(97, 97, 97, 0.15);
+    border-radius: 4px;
+    transition: all 300ms ease-in;
+  }
 
+  & > span:hover {
+    cursor: pointer;
+    background: rgba(244, 143, 177, 0.15);
+    border: 1px solid ${colors.DarkPink};
+  }
+
+  .active {
+    cursor: pointer;
+    background: rgba(244, 143, 177, 0.15);
+    border: 1px solid ${colors.DarkPink};
+  }
 `;
 
 export default Pagination
