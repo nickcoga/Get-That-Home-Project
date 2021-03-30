d150c5b2326accae1ea336a2cab6f60b6d8165ec
Abel AR <abel_john_14@hotmail.com>
2021-03-29 23:58:25 -0500

    Add Cards properties

--- src/Components/LandPage/LandPage.js
+++ src/Components/LandPage/LandPage.js
@@ -5,15 +5,16 @@ import Text from '../styles/Heading';
 import '../CardComponentProperty/CardComponentProperty.css'
 import NavbarWelcome from '../NavbarWelcome/NavbarWelcome';
 import NavbarLanding from '../UI/NavbarLanding';
+import CardComponentProperty from '../CardComponentProperty/CardComponentProperty';
 
 const LandPageStyles=styled.div`
-    background: red;
     width: 100%;
     
     .landPage {
         width: 100%;
         display: flex;
-        justify-content: center;
+        flex-direction: column;
+        align-items: center;
         margin: 60px 0 0 0;
         .secondHeader {
             position: absolute;
@@ -28,6 +29,22 @@ const LandPageStyles=styled.div`
     
         img {
             width: 100%;
+        }
+
+        .box-cards {
+            box-sizing: border-box;
+            width: 100%;
+            padding: 64px 222px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+            .box-cards--properties {
+                width: 100%;
+                display: flex;
+                padding: 8px 0 0 0;
+            }
+
         }
     }
     
@@ -46,6 +63,17 @@ const LandPage = () => {
                     </div>
                 </div>
                 <img src={LandPageImage} alt="Buildings" />
+                <div className="box-cards">
+                    <Text type='Montserrat' size='Subtitle2'>Find an Apartment you Love</Text>
+                    <Text type='Montserrat' size='H4' color='DarkPink'>Homes for rent at the best prices</Text>
+                    
+                    {/* ¡IMPORTANTE!: por el momento esto ira asi, despues le pondre el map */}
+                    <div className="box-cards--properties">
+                        <CardComponentProperty />
+                        <CardComponentProperty />
+                        <CardComponentProperty />
+                    </div>
+                </div>
             </div>
         </LandPageStyles>
     )
