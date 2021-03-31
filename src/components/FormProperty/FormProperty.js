138815a9b23d7185595c298315073d7d8f165116
Abel AR <abel_john_14@hotmail.com>
2021-03-31 11:07:32 -0500

    create form property

---
+++ src/components/FormProperty/FormProperty.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormProperty = () => {
+    return (
+        <div>
+            <h1>Form Property for rent or sale</h1>
+        </div>
+    )
+}
+
+export default FormProperty
