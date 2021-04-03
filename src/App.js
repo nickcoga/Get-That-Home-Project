1548b22af89ffb97a4afb4e685c732bc039c34f4
Nick Correa <nickcorreagarcia@gmail.com>
2021-04-03 14:18:01 -0500

    Created InputPlaceAutocomplete with google place Api and .env

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import { BrowserRouter as Router, Route } from "react-router-dom";
 import React from "react";
-import FormProperty from "./components/FormProperty/FormProperty";
+import SearchLocationInput from "./components/InputPlace/InputPlacesAutocomplete";
+// import FormProperty from "./components/FormProperty/FormProperty";
 // import LandPage from "./pages/LandPage";
 // import Login from "./pages/Login";
 
@@ -13,7 +14,8 @@ function App() {
           <Route path="/login" component={Login} />
         </div>
       </Router> */}
-      <FormProperty />
+      {/* <FormProperty /> */}
+      <SearchLocationInput onChange={() => null} />
     </div>
   );
 }
