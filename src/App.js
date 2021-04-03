9c484d9f642510d9a4d1b62a02a636cf3b9fe837
christian <cristian14021997@gmail.com>
2021-04-03 12:56:29 -0500

    Adding routes to property detail

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import LandPage from "./pages/LandPage";
 import Signup from "./pages/Signup";
 import ListProperties from "./pages/ListProperties";
 import SavedProperties from "./pages/SavedProperties";
+import PropertyForm from "./pages/PropertyForm";
+import PropertyDetail from "./pages/PropertyDetail";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
           <Route path="/signup" component={Signup} />
           <Route path="/listproperties" component={ListProperties} />
           <Route path="/saveproperties" component={SavedProperties} />
+          <Route path="/propertyform" component={PropertyForm} />
+          <Route path="/propertydetail" component={PropertyDetail} />
         </div>
       </Router>
       {/* <FormProperty /> */}
---
+++ src/pages/PropertyDetail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PropertyDetail = () => {
+  return (
+    <div>
+      <h1>Property detail</h1>
+    </div>
+  );
+};
+
+export default PropertyDetail;
