3c7d263bf57518377e78510494f4e55373a684b0
christian <cristian14021997@gmail.com>
2021-04-03 12:12:20 -0500

    Adding route for signup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-// import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import React from "react";
-import FormProperty from "./components/FormProperty/FormProperty";
-// import LandPage from "./pages/LandPage";
-// import Login from "./pages/Login";
+// import FormProperty from "./components/FormProperty/FormProperty";
+import LandPage from "./pages/LandPage";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <div>
-      {/* <Router>
+      <Router>
         <div>
           <Route exact path="/" component={LandPage} />
-          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
         </div>
-      </Router> */}
-      <FormProperty />
+      </Router>
+      {/* <FormProperty /> */}
     </div>
   );
 }
