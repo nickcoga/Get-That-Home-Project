3f0ec3e918b8198cb0676918f098eb015f26bb06
Nick Correa <nickcorreagarcia@gmail.com>
2021-04-04 23:53:02 -0500

    Resolving conflicts App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import React from "react";
-// import LandPage from "./pages/LandPage";
-// import Signup from "./pages/Signup";
-// import ListProperties from "./pages/ListProperties";
-// import SavedProperties from "./pages/SavedProperties";
-// import PropertyForm from "./pages/PropertyForm";
-// import PropertyDetail from "./pages/PropertyDetail";
-import FormProperty from "./components/FormProperty/FormProperty";
-// import Maps from "./components/Maps/Maps";
-// import SearchLocationInput from "./components/InputPlace/InputPlacesAutocomplete";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LandPage from "./pages/LandPage";
+import Signup from "./pages/Signup";
+import ListProperties from "./pages/ListProperties";
+import SavedProperties from "./pages/SavedProperties";
+import PropertyForm from "./pages/PropertyForm";
+import PropertyDetail from "./pages/PropertyDetail";
 
 function App() {
   return (
-    // <Router>
-    //   <Switch>
-    //     <Route exact path="/" component={LandPage} />
-    //     <Route path="/signup" component={Signup} />
-    //     <Route path="/listproperties" component={ListProperties} />
-    //     <Route path="/saveproperties" component={SavedProperties} />
-    //     <Route path="/propertyform" component={PropertyForm} />
-    //     <Route path="/propertydetail" component={PropertyDetail} />
-    //   </Switch>
-    // </Router>
-    <FormProperty />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandPage} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/listproperties" component={ListProperties} />
+        <Route path="/saveproperties" component={SavedProperties} />
+        <Route path="/propertyform" component={PropertyForm} />
+        <Route path="/propertydetail" component={PropertyDetail} />
+      </Switch>
+    </Router>
   );
-
-  {
-    /* <SearchLocationInput onChange={() => null} /> */
-  }
-  {
-    /* <Maps /> */
-  }
 }
 
 export default App;
