dc4699f5622085087fd700dad71c13abee6e1d3b
Abel AR <abel_john_14@hotmail.com>
2021-04-04 19:29:05 -0500

    update branch

--- src/app/Store.js
+++ src/app/Store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import SessionSlice from "../components/Features/Session/SessionSlice";
+import SessionSlice from "../components/Features/session/SessionSlice";
 import UsersReducer  from "../components/Features/Users/UsersSlice";
 
 export default configureStore({
